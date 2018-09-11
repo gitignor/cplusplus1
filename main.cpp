52e7f4ad677d44d172923b09aaed7ee5bdb63648
Aleksei Nikiforov <a.nikiforov@negative.ee>
2018-09-11 18:23:00 +0300

    Second home work 17TARgv

--- main.cpp
+++ main.cpp
@@ -1,75 +1,52 @@
-/*
+/* Aleksei Nikiforov 17TARgv
  * ABC
  * Существует треугольник?
  * Какой?
  * float
  * A>B+C
- *
+ * Площадь
  * */
-
 
 #include <QCoreApplication>
 #include <QDebug> //F1 for help
 #include <QTextStream>
 #include <QString>
-#include "switchgender.cpp"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication ann(argc, argv);
     QTextStream cin(stdin);
-    QTextStream cout(stdout);
-    qDebug() << "Hello TAR vg 17!" ; // Qt debug output to console
 
-    while (mainFunc()) {
-        qDebug() << "While body";
+    float a,b,c;
+    QString askTriangle = "Введите длины трех сторон треугольника";
+    qDebug() << askTriangle;
+    cin >> a >> b >> c;
+    QString triangleSizes = "Размеры сторон треугольника: %1x%2x%3";
+    triangleSizes = triangleSizes.arg(a).arg(b).arg(c);
+    qDebug() << triangleSizes;
+    if (((a + b > c) && (a + c > b) && (b + c > a)) && ((a > 0) && (b > 0) && (c > 0))){
+         qDebug() << "Треугольник существует";
+    } else {
+         qDebug() << "Треугольник не существует";
+         return 0;
     }
-
-    /*for (;;) {
-            mainFunc();
-        }*/
-
-#if 0 // fast way to disable code
-    qDebug() << "Let's pull it!";
-    int a, b;
-    int n, m, l;
-    for (;;) {
-        qDebug() << "Please enter size of brick";
-        cin >> n >> m >> l;
-        QString qsSizes = "size of brick is %1x%2x%3";
-        qsSizes = qsSizes.arg(n).arg(m).arg(l);
-        qDebug() << qsSizes;
-        if ((n == 0) || (m==0) || (l==0)) return 0;
-
-        qDebug() << "Please enter size of hole";
-        cin >> a >> b;
-        qsSizes = "size of hole is %1x%2" ;
-        qsSizes = qsSizes.arg(a).arg(b);
-        qDebug() << qsSizes;
-        if ((a == 0) || (b==0)) return 0;
-
-        // First check brick ( put on M side ): 20x3x4  hole: 3x5
-        {
-            qDebug() << "First check brick ( put on M side ):";
-            if ((l<a) && (n<b)) qDebug() << "fits 1.1";
-            if ((l<b) && (n<a)) qDebug() << "fits 1.2";
-        }
-
-        // Second check ( put on N side ),
-        {
-            qDebug() << "Second check brick ( put on N side ):";
-            if ((m<a) && (l<b)) qDebug() << "fits 2.1";
-            if ((m<b) && (l<a)) qDebug() << "fits 2.2";
-        }
-
-        // Last check ( put on L side )
-        {
-            qDebug() << "Last check brick ( put on L side ):";
-            if ((m<a) && (n<b)) qDebug() << "fits 2.1";
-            if ((m<b) && (n<a)) qDebug() << "fits 2.2";
-        }
+    float p = (a + b + c) / 2; //находим полупериметр
+    float s = sqrt(p * (p - a) * (p - b) * (p - c)); // находим площадь
+    QString triangleType = ""; // переменная, куда будем записывать тип нашего тре-ка
+    if (a == b && b == c) {
+        triangleType = "равностороннего ";
+    } else if (a == b || b == c || a == c) {
+        triangleType = "равнобедренного ";
+    } else {
+        triangleType = "разностороннего ";
+    } if (a * a + b * b == c * c || a * a + c * c == b * b || c * c + b * b == a * a) {
+        triangleType += "прямоугольного ";
+    } else if ((a * a + b * b > c * c || a * a + c * c > b * b || c * c + b * b > a * a) && (a != b && b != c && a != c)) {
+        triangleType += "тупого ";
+    } else {
+        triangleType += "острого ";
     }
-#endif // end of code disable
+    qDebug() << "Площадь " + triangleType + "треугольника = " << s << "\" квадратным единицам";
 
     return ann.exec();
 }
